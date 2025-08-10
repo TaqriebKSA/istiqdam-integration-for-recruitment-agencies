70a191ec4c4cea24a915145d5c713c6d50bf644b
Shahzad Naser <shahzadnaser1122@gmail.com>
2025-08-10 11:41:04 +0500

    Add integrations logs dcotype

---
+++ istiqdam_integration/istiqdam_integration/doctype/integration_logs/integration_logs.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Shahzad Naser and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Integration Logs", {
+// 	refresh(frm) {
+
+// 	},
+// });
